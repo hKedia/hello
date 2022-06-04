879df071465d10119f64664fd5689b2c3effb321
harsh.kedia <harsh.kedia@alcemy.tech>
2022-06-04 16:39:28 +0200

    update dev config use local pg container

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,11 @@ import Config
 
 # Configure your database
 config :hello, Hello.Repo,
-  url: System.get_env("DATABASE_URL"),
+  username: "postgres",
+  password: "postgrespw",
+  hostname: "localhost",
+  port: 55000,
+  database: "hello_dev",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
@@ -15,7 +19,7 @@ config :hello, Hello.Repo,
 config :hello, HelloWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
